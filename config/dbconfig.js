d2262d55107fbc119ee10308e50a22f96cea73cb
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2021-08-01 20:02:43 +0200

    Move config to different folder, remove unused packages, split routes to different base urls and start env on app initialization

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,5 @@
 import admin from 'firebase-admin'
 import fs from 'fs'
-import path from 'path'
-import dotenv from 'dotenv';
-dotenv.config({ path: path.resolve('credentials/.env') });
 
 export default function config() {
     const serviceAccount = fs.readFileSync(new URL('admin.json', import.meta.url), 'utf8')
--- routing/router.js
+++ routing/router.js
@@ -2,8 +2,12 @@ import { authRouter } from './auth.js'
 import { checklistRouter } from './checklist.js'
 import { userRouter } from './user.js'
 
-export const routes = [
+export const publicRoutes = [
     authRouter,
     checklistRouter,
     userRouter
+]
+
+export const protectedRoutes = [
+    checklistRouter
 ]
